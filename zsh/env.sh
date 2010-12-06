4bbdc1f270294d518cf9df26bbd7ff098abc7165
Blake Taylor <btaylor@agoragames.com>
2010-12-06 09:42:05 -0500

    Changed Paths

--- zsh/env.sh
+++ zsh/env.sh
@@ -1,5 +1,5 @@
-# Add missing paths
-export PATH=$PATH:/usr/local/sbin:.
+# Setup Path
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:.
 
 # Include gem directory in path and setup RVM. Also add current directory to path.
 export PATH=`gem env gemdir`:$PATH
